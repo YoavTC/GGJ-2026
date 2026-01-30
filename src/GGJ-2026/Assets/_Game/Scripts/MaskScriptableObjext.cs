37de9047636a18a03e93d2a1eae9017403b119f5
Eyal Label <sslabel2@gmail.com>
2026-01-30 14:06:06 +0200

    Mask Scriptable Objects

--- src/GGJ-2026/Assets/_Game/Scripts/MaskScriptableObjext.cs
+++ src/GGJ-2026/Assets/_Game/Scripts/MaskScriptableObjext.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "MaskScriptableObjext", menuName = "Scriptable Objects/MaskScriptableObjext")]
+[CreateAssetMenu(fileName = "Masks")]
 public class MaskScriptableObjext : ScriptableObject
 {
+    public MaskType MaskType;
+    public GameObject MaskPrefab;
+    public string MaskName;
+    public string Description;
     
+}
+
+public enum MaskType
+{
+    HEAVY,
+    DEFLECT,
+    DASH,
+    ANCHOR,
+    KAMIKAZE
 }
